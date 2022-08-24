bc2761f8d0e0e49b221119d671b17f8453d3b2a7
Matheus Luís Webber <matheus.webber@hotmail.com>
2022-08-23 22:34:12 -0300

    Implementado o algoritmo DES de criptografia

--- core/main.py
+++ core/main.py
@@ -9,16 +9,17 @@ if __name__ == '__main__':
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
     
-    AES.encrypt('dados.txt', RSA.import_key(open("core/chaves_RSA/public.pem", "rb").read()), 128)
+    # AES.encrypt('dados.txt', RSA.import_key(open("core/chaves_RSA/public.pem", "rb").read()), 128)
 
-    AES.decrypt("encrypted_data.base64", "encrypted_key.base64", RSA.import_key(open("core/chaves_RSA/private.pem", "rb").read()))
+    # AES.decrypt("encrypted_data.base64", "encrypted_key.base64", RSA.import_key(open("core/chaves_RSA/private.pem", "rb").read()))
 
     # if "-cipher" in opts:
     #     if "aes" == args[1]:
-    #         Env = Envelope("aes", int(args[2]))
-    #         Env.cipher(args[0], RSA.import_key(open("public.pem", 'rb').read()))
+    #         AES.encrypt(args[0], RSA.import_key(open("public.pem", 'rb').read()), 128)
 
     # if "-decipher" in opts:
     #     if "aes" == args[2]:
-    #         Env = Envelope("aes", 0)
-    #         Env.decipher(args[0], args[1], RSA.import_key(open("private.pem", 'rb').read()), args[3])
+    #         AES.decrypt(args[0], args[1], RSA.import_key(open("private.pem", 'rb').read()))
+
+    DES.encrypt('dados.txt', RSA.import_key(open("core/chaves_RSA/public.pem", "rb").read()))
+    DES.decrypt("encrypted_data.base64", "encrypted_key.base64", RSA.import_key(open("core/chaves_RSA/private.pem", "rb").read()))
